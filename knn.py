aec216e83d05bf8f18a06369063be13d52019e3b
Javier Moreno <javmorenov@gmail.com>
2017-11-21 20:07:16 +0100

    KNN

--- knn.py
+++ knn.py
@@ -32,11 +32,10 @@ def get_hists_of_images_node(node):
     directory = 'Frames/Nodo_' + str(node)
     hists = []
 
-    for i in range(1, 5):
+    for i in range(1, 2):
         imagePath = directory + '/imagen' + str(i) + '.jpg'
         image = cv2.imread(imagePath, 0)
-        hist = image
-        #hist = get_hist(image)
+        hist = get_hist(image)
         hists.append(hist)
 
     return hists
@@ -46,21 +45,30 @@ print 'Loading images...'
 responses = []
 trainData = []
 
-for i in range(1, 8):
+for i in range(1, 2):
     hists = get_hists_of_images_node(i)
-    responses = np.concatenate((responses, [i, i, i, i, i]))
+    responses = np.concatenate((responses, [i]))
+    print hists
     trainData.append(hists)
+    print trainData
+    trainData = np.concatenate((trainData, hists))
 
-responses = np.float32(responses)
 trainData = np.float32(trainData)
+responses = np.float32(responses)
 
 print 'Train KNN...'
 knn = cv2.ml.KNearest_create()
+
+'''trainData = np.float32([
+    [[1,3,4], [2,5,6], [3,3,2], [4,5,4]],
+    [[4,2,3], [5,4,5], [7,5,6], [8,2,3]]
+])'''
+
 knn.train(trainData, cv2.ml.ROW_SAMPLE, responses)
 
 print 'Finding...'
-ret, results, neighbours ,dist = knn.find_nearest(trainData[0][0], 3)
+#ret, results, neighbours ,dist = knn.find_nearest(trainData[0][0], 3)
 
-print "result: ", results,"\n"
-print "neighbours: ", neighbours,"\n"
-print "distance: ", dist
+#print "result: ", results,"\n"
+#print "neighbours: ", neighbours,"\n"
+#print "distance: ", dist
