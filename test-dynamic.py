ed04c5d589c9adf71136e275b61e2dfda93af8b3
Javier Moreno <jmorenov28@gmail.com>
2017-11-27 11:09:43 +0100

    Using videos to test

--- test-dynamic.py
+++ test-dynamic.py
@@ -4,12 +4,23 @@ import videos
 import cv2
 import numpy as np
 
-def get_named_node(result):
-    name = ''
-    if results[0][0] != -1:
-        name = 'Nodo ' + str(int(results[0][0]))
+def get_named_nodes(actual_node, next_node):
+    actual_node_name = 'Actual Nodo ' + str(int(actual_node))
+    next_node_name = 'Siguiente Nodo ' + str(int(next_node))
 
-    return name
+    return actual_node_name, next_node_name
+
+def compute_node_detected(node_detected, result, times_detected):
+    if result == -1:
+        node_detected = -1
+        times_detected = 0
+    elif node_detected == -1:
+        node_detected = result
+        times_detected = 1
+    elif node_detected == result:
+        times_detected += 1
+
+    return node_detected, times_detected
 
 FRAMES_PER_NODE = 115
 NUMBER_OF_NODES = 7
@@ -23,6 +34,10 @@ knn_model = knn.train(data, responses)
 
 print 'Predict...'
 cap = videos.load_video(TEST_VIDEO)
+actual_node = 1
+next_node = actual_node + 1
+node_detected = -1
+times_detected = 0
 
 while(cap.isOpened()):
     ret, frame = cap.read()
@@ -33,11 +48,21 @@ while(cap.isOpened()):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     hist = images.get_hist(gray)
     sample = np.array([hist])
-    retu, results, neighbours, dist = knn.predict(knn_model, sample, 9, max_distance=6)
+    retu, results, neighbours, dist = knn.predict(knn_model, sample, 9, max_distance=3.5)
+    node_detected, times_detected = compute_node_detected(node_detected, results[0][0], times_detected)
     frame_rotated = images.rotate_image(frame, -90)
-    named_node = get_named_node(results[0][0])
 
-    cv2.putText(frame_rotated, named_node, (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 6, (200, 255, 155), 13, cv2.LINE_AA)
+    if node_detected != -1:
+        print 'Nodo predecido: ' + str(int(node_detected))
+
+        if node_detected == next_node or (node_detected > actual_node and times_detected >= 7):
+            actual_node = node_detected
+            next_node = actual_node + 1
+
+    actual_node_name, next_node_name = get_named_nodes(actual_node, next_node)
+
+    cv2.putText(frame_rotated, actual_node_name, (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 3, (200, 255, 155), 13, cv2.LINE_AA)
+    cv2.putText(frame_rotated, next_node_name, (10, 1000), cv2.FONT_HERSHEY_SIMPLEX, 3, (200, 255, 155), 13, cv2.LINE_AA)
     cv2.namedWindow('Test Dynamic', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('Test Dynamic', 600, 600)
     cv2.imshow('Test Dynamic', frame_rotated)
