e8e03c655fd0cb8f96c1a9fdd068f3b40decacd2
Javier Moreno <jmorenov28@gmail.com>
2017-11-27 12:31:32 +0100

    Working with cross validation

--- run-cross-validation.py
+++ run-cross-validation.py
@@ -1,6 +1,9 @@
 import images
 import knn
 import crossvalidation
+import timeit
+
+start = timeit.default_timer()
 
 FRAMES_PER_NODE = 115
 NUMBER_OF_NODES = 7
@@ -12,4 +15,8 @@ print 'Train KNN...'
 knn_model = knn.train(data, responses)
 
 print 'Cross validation...'
-crossvalidation.loo(knn_model, data, responses, 19)
+crossvalidation.loo(knn_model, data, responses, 3)
+
+stop = timeit.default_timer()
+
+print 'Execution time: ' + str(stop - start) + 's'
